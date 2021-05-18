2814d9adbeac16533930e37f1e36e491d716639c
[Andrew Core] <[adcore89@gmail.com]>
2021-05-17 21:21:58 -0400

    category route started

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,6 +5,12 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll()
+  .then(dbUserData => res.json(dbCategoryDat))
+  .catch(err=> {
+    console.log(err);
+    res.status(500).json(err);
+  })
   // be sure to include its associated Products
 });
 
