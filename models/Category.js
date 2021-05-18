f28fa2f5e33e029552830a5031ab84f6391efe21
[Andrew Core] <[adcore89@gmail.com]>
2021-05-17 21:11:26 -0400

    added category model

--- models/Category.js
+++ models/Category.js
@@ -4,9 +4,29 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+// model to include
+// ID
+// Int
+// no null 
+// primary key 
+// auto inc 
+// category_name 
+// string 
+// doesnt allow null value 
+
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+//define category name 
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
--- models/Product.js
+++ models/Product.js
@@ -7,6 +7,27 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
+// Product
+// id
+// Integer
+// Doesn't allow null values
+// Set as primary key
+// Uses auto increment
+// product_name
+// String
+// Doesn't allow null values
+// price
+// Decimal
+// Doesn't allow null values
+// Validates that the value is a decimal
+// stock
+// Integer
+// Doesn't allow null values
+// Set a default value of 10
+// Validates that the value is numeric
+// category_id
+// Integer
+// References the category model's id
 Product.init(
   {
     // define columns
